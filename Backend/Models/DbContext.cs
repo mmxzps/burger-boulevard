cabf6b3929abe1da7799d173f3c0a5fab442b521
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-03-29 11:20:11 +0100

    DbContext DbSet required and reorder

--- Backend/Models/DbContext.cs
+++ Backend/Models/DbContext.cs
@@ -5,17 +5,17 @@ namespace Backend;
 
 public class BackendContext : DbContext
 {
-  public DbSet<Menu> Menus { get; set; }
-  public DbSet<Product> Products { get; set; }
-  public DbSet<Price> Prices { get; set; }
-  public DbSet<Category> Categories { get; set; }
-  public DbSet<Order> Orders { get; set; }
-  public DbSet<OrderProduct> OrderProducts { get; set; }
-  public DbSet<BaseProductIngredient> BaseProductIngredients { get; set; }
-  public DbSet<PossibleProductIngredient> PossibleProductIngredients { get; set; }
-  public DbSet<ModifiedOrderProductIngredient> ModifiedOrderProductIngredients { get; set; }
-  public DbSet<Ingredient> Ingredients { get; set; }
-  public DbSet<FeaturedProduct> FeaturedProducts { get; set; }
+  public required DbSet<Ingredient> Ingredients { get; set; }
+  public required DbSet<Product> Products { get; set; }
+  public required DbSet<ProductIngredient> ProductIngredients { get; set; }
+  public required DbSet<Category> Categories { get; set; }
+  public required DbSet<Price> Prices { get; set; }
+  public required DbSet<Sale> Sales { get; set; }
+  public required DbSet<Menu> Menus { get; set; }
+  public required DbSet<FeaturedProduct> FeaturedProducts { get; set; }
+  public required DbSet<Order> Orders { get; set; }
+  public required DbSet<OrderProduct> OrderProducts { get; set; }
+  public required DbSet<ModifiedOrderProductIngredient> ModifiedOrderProductIngredients { get; set; }
 
   public BackendContext(DbContextOptions<BackendContext> options) : base(options) {}
 }
