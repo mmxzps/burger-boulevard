19c4d41d6bbed0b9a457a4b598d5f9740577d918
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-03-29 11:30:10 +0100

    Remove OrderProduct properties from other entities

--- Backend/Models/Entities/ModifiedOrderProductIngredient.cs
+++ Backend/Models/Entities/ModifiedOrderProductIngredient.cs
@@ -9,7 +9,6 @@ public enum ProductModification
 public class ModifiedOrderProductIngredient
 {
   public int Id { get; set; }
-  public required OrderProduct OrderProduct { get; set; }
   public required Ingredient Ingredient { get; set; }
   public required ProductModification Modification { get; set; }
 }
--- Backend/Models/Entities/Product.cs
+++ Backend/Models/Entities/Product.cs
@@ -7,6 +7,5 @@ public class Product
   public required string Description { get; set; }
   public required Price Price { get; set; }
   public required Category Category { get; set; }
-  public required List<OrderProduct> OrderProducts { get; set; }
   public required List<ProductIngredient> ProductIngredients { get; set; }
 }
