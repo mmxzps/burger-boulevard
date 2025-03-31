3a869811a052d6ae57cd8857c9cfe96ac5646314
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-03-31 14:54:42 +0200

    Re-add mistakenly removed property Product to ProductIngredient

--- Backend/Models/Entities/ProductIngredient.cs
+++ Backend/Models/Entities/ProductIngredient.cs
@@ -3,6 +3,7 @@
 public class ProductIngredient
 {
   public int Id { get; set; }
+  public required Product Product { get; set; }
   public uint BaseAmount { get; set; }
   public uint MinAmount { get; set; }
   public uint MaxAmount { get; set; }
