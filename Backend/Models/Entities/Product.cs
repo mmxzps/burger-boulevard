984b27a9be5e00f617007f470b1a832fda7af17a
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-03-31 14:53:27 +0200

    Separate Price and Discount on Product

--- Backend/Models/Entities/Product.cs
+++ Backend/Models/Entities/Product.cs
@@ -5,7 +5,8 @@ public class Product
   public int Id { get; set; }
   public required string Name { get; set; }
   public required string Description { get; set; }
-  public required Price Price { get; set; }
   public required Category Category { get; set; }
   public required ICollection<ProductIngredient> ProductIngredients { get; set; }
+  public required Price Price { get; set; }
+  public required Discount Discount { get; set; }
 }
