71bbe551a343d1f2ede7673eb1126f1045b942a3
niklas klaar <nickezklaar@gmail.com>
2025-04-01 10:22:06 +0200

    removed discount from price

--- Backend/Models/Entities/Price.cs
+++ Backend/Models/Entities/Price.cs
@@ -5,7 +5,5 @@ namespace Backend.Models.Entities;
 public class Price
 {
   public int Id { get; set; }
-  [Precision(8, 4)]
-  public decimal BasePrice { get; set; }
-  public required Discount Discount { get; set; }
+  [Precision(8, 4)] public decimal BasePrice { get; set; }
 }
