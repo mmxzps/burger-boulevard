7ff59e4970ba9377b56a8c9c2ecc5422505353c4
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-04-07 20:15:03 +0200

    Add Components controller Get and Featured endpoints

--- Backend/Controllers/Components.cs
+++ Backend/Controllers/Components.cs
@@ -18,7 +18,7 @@ public class Components : ControllerBase
     };
 
   [HttpGet]
-  public ActionResult<IEnumerable<Component>> Get(
+  public ActionResult<IEnumerable<Component>> Find(
       BackendContext context,
       Models.Entities.ComponentLevel? level,
       int? categoryId) =>
@@ -30,5 +30,25 @@ public class Components : ControllerBase
         .Where(c =>
           (categoryId == null || c.Categories.Any(cat => cat.Id == categoryId)) &&
           (level == null || c.Level == level))
+        .Select(c => c.ToDto()));
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Component>> Get(BackendContext context, int id) =>
+    Ok((await context.Components
+          .Include(c => c.Categories)
+          .Include(c => c.ChildPolicies)
+          .Include(c => c.Price)
+          .FirstOrDefaultAsync(c => c.Id == id))?.ToDto());
+
+  [HttpGet("featured")]
+  public ActionResult<IEnumerable<Component>> Featured(BackendContext context) =>
+    Ok(context.FeaturedComponents
+        .AsNoTracking()
+        .Include(c => c.Component)
+        .ThenInclude(c => c.Categories)
+        .Include(c => c.Component)
+        .ThenInclude(c => c.ChildPolicies)
+        .Include(c => c.Component)
+        .ThenInclude(c => c.Price)
         .Select(c => c.ToDto()));
 }
