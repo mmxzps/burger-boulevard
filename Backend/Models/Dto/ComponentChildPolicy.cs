8e610af8536116e4767d9d3dda35b7f6ad78ad06
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-04-09 18:32:57 +0200

    Remove ComponentChildPolicy DTO property Parent

--- Backend/Models/Dto/ComponentChildPolicy.cs
+++ Backend/Models/Dto/ComponentChildPolicy.cs
@@ -3,7 +3,6 @@ namespace Backend.Models.Dto;
 public class ComponentChildPolicy
 {
   public int Id { get; set; }
-  public required Component Parent { get; set; }
   public required Component Child { get; set; }
 
   public int Default { get; set; }
