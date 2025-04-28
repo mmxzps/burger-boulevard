f61b5d7d07b812e7f1137d2657f99afc8a09a251
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-04-28 12:21:22 +0200

    Remove DisplayOrderIndex

--- Backend/Models/Entities/Component.cs
+++ Backend/Models/Entities/Component.cs
@@ -2,44 +2,41 @@ namespace Backend.Models.Entities;
 
 public enum ComponentLevel
 {
-  Ingredient = 0,
-  Product = 1,
-  Menu = 2
+    Ingredient = 0,
+    Product = 1,
+    Menu = 2
 }
 
 public class Component : IIntoDto<Dto.Component>
 {
-  public int Id { get; set; }
-  public ComponentLevel Level { get; set; }
-  public required string Name { get; set; }
-	public string? Description { get; set; }
-  public int? ImageId { get; set; }
-  public required Image? Image { get; set; }
-  public string? ImageUrl =>
-    ImageId is int id ? $"/api/Images/{id}" : null;
-	public virtual ICollection<Category> Categories { get; set; } = [];
+    public int Id { get; set; }
+    public ComponentLevel Level { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public int? ImageId { get; set; }
+    public required Image? Image { get; set; }
+    public string? ImageUrl =>
+      ImageId is int id ? $"/api/Images/{id}" : null;
+    public virtual ICollection<Category> Categories { get; set; } = [];
 
-  public virtual ICollection<ComponentChildPolicy> ChildPolicies { get; set; } = [];
+    public virtual ICollection<ComponentChildPolicy> ChildPolicies { get; set; } = [];
 
-  public virtual ICollection<OrderComponent> OrderComponents { get; set; } = [];
+    public virtual ICollection<OrderComponent> OrderComponents { get; set; } = [];
 
-  public decimal CurrentPrice =>
-    Price.BasePrice * (Discount?.Multiplier ?? 1);
-  public required Price Price { get; set; }
-	public required Discount? Discount { get; set; }
+    public decimal CurrentPrice =>
+      Price.BasePrice * (Discount?.Multiplier ?? 1);
+    public required Price Price { get; set; }
+    public required Discount? Discount { get; set; }
 
-  public int? DisplayOrderIndex { get; set; } // Only useful for level 0 components.
-
-  public Dto.Component ToDto() => new Dto.Component
-  {
-    Id = Id,
-    Level = Level,
-    Name = Name,
-    Description = Description,
-    ImageUrl = ImageUrl,
-    Categories = Categories.Select(c => c.ToDto()),
-    OriginalPrice = Price.BasePrice,
-    Discount = Discount?.Multiplier,
-    DisplayOrderIndex = DisplayOrderIndex
-  };
+    public Dto.Component ToDto() => new Dto.Component
+    {
+        Id = Id,
+        Level = Level,
+        Name = Name,
+        Description = Description,
+        ImageUrl = ImageUrl,
+        Categories = Categories.Select(c => c.ToDto()),
+        OriginalPrice = Price.BasePrice,
+        Discount = Discount?.Multiplier
+    };
 }
