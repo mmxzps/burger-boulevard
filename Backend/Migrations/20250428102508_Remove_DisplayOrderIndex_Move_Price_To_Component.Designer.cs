5f432596caf1f28ed065fb67b05a6de25a60dda5
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-04-28 12:27:13 +0200

    Migrations

--- Backend/Migrations/20250428102508_Remove_DisplayOrderIndex_Move_Price_To_Component.Designer.cs
+++ Backend/Migrations/20250428102508_Remove_DisplayOrderIndex_Move_Price_To_Component.Designer.cs
@@ -4,6 +4,7 @@ using Backend;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(BackendContext))]
-    partial class BackendContextModelSnapshot : ModelSnapshot
+    [Migration("20250428102508_Remove_DisplayOrderIndex_Move_Price_To_Component")]
+    partial class Remove_DisplayOrderIndex_Move_Price_To_Component
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
