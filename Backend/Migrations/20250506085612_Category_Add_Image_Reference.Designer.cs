5fa126896e0781543f0ccb6b9c13b7427bcfbefd
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-05-06 19:04:39 +0200

    feat(api): category images

--- Backend/Migrations/20250506085612_Category_Add_Image_Reference.Designer.cs
+++ Backend/Migrations/20250506085612_Category_Add_Image_Reference.Designer.cs
@@ -4,6 +4,7 @@ using Backend;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(BackendContext))]
-    partial class BackendContextModelSnapshot : ModelSnapshot
+    [Migration("20250506085612_Category_Add_Image_Reference")]
+    partial class Category_Add_Image_Reference
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Backend/Models/Entities/Category.cs
+++ Backend/Models/Entities/Category.cs
@@ -4,11 +4,16 @@ public class Category : IIntoDto<Dto.Category>
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public int? ImageId { get; set; }
+    public required Image? Image { get; set; }
+    public string? ImageUrl =>
+      ImageId is int id ? $"/api/Images/{id}" : null;
     public virtual ICollection<Component> Components { get; set; } = [];
 
     public Dto.Category ToDto() => new Dto.Category
     {
         Id = Id,
-        Name = Name
+        Name = Name,
+        ImageUrl = ImageUrl
     };
 }
